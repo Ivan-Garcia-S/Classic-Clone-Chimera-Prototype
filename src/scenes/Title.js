2beea036d83b987256785c9aae6dafbfab4a3d1d
Robert Williams <roewilli@ucsc.edu>
2021-10-17 14:08:25 -0700

    Flipped the blue player image for title

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -7,7 +7,7 @@ class Title extends Phaser.Scene {
         this.load.path = './assets/'; 
         this.load.spritesheet('Player1', 'PlayerRed.png', {frameWidth: 32, frameHeight: 32});
         this.load.image('p2', 'Player_2.png');
-        this.load.spritesheet('Player2', 'PlayerBlue.png', {frameWidth: 32, frameHeight: 32});
+        this.load.spritesheet('Player2', 'PlayerBlueF.png', {frameWidth: 32, frameHeight: 32});
         this.load.image('tempBG', 'tempBG.png');
     }
     create(){
@@ -40,58 +40,10 @@ class Title extends Phaser.Scene {
         this.add.text(game.config.width/2, game.config.height/2 + 150, 'Collect all coins to win the game and kill the ghosts with your sword.').setOrigin(0.5);
         
         this.add.image(150, game.config.height/2, 'Player1').setOrigin(0.5).setScale(2);
-        this.p2 = new Player(this, 750, game.config.height/2, keyW, keyS, keyA, keyD, keyENTER, 'Player2').setScale(2);
-        this.p2.flipX = true;
+        this.add.image(750, game.config.height/2,'Player2').setOrigin(0.5).setScale(2);
+        
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        //Create Player Animations
-        this.anims.create({
-            key: 'idle1',
-            frameRate: 8,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('Player1', { start: 0, end: 19 }),
-        });
-        this.anims.create({
-            key: 'idle2',
-            frameRate: 8,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('Player2', { start: 0, end: 19 }),
-        });
-        this.anims.create({
-            key: 'attack1',
-            frameRate: 16,
-            repeat: 0,
-            frames: this.anims.generateFrameNumbers('Player1', { start: 20, end: 31 }),
-        });
-        this.anims.create({
-            key: 'attack2',
-            frameRate: 16,
-            repeat: 0,
-            frames: this.anims.generateFrameNumbers('Player2', { start: 20, end: 31 }),
-        });
-        this.anims.create({
-            key: 'walk1',
-            frameRate: 8,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('Player1', { start: 32, end: 41 }),
-        });
-        this.anims.create({
-            key: 'walk2',
-            frameRate: 8,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('Player2', { start: 32, end: 41 }),
-        });
-        this.anims.create({
-            key: 'throw1',
-            frameRate: 8,
-            repeat: 0,
-            frames: this.anims.generateFrameNumbers('Player1', { start: 42, end: 43 }),
-        });
-        this.anims.create({
-            key: 'throw2',
-            frameRate: 8,
-            repeat: 0,
-            frames: this.anims.generateFrameNumbers('Player2', { start: 42, end: 43 }),
-        });
+        
 
     }
     update(){
